df2119a3e2345d1fb034635d67255e181d097317
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-06 09:04:39 -0500

    Done

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "github.com/prasannayanamadala/projects/calculator"
 
 func main() {
 	
-	//operations.AddOperation()
-	//operations.SubOperation()
-	//operations.MulOperation()
+	operations.AddOperation()
+	operations.SubOperation()
+	operations.MulOperation()
 	operations.DivideOperation()
 	
 }
