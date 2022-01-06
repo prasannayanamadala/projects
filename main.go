4fa043a27eecd09c71e428e2cbcdfe8dd7811438
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-06 01:38:37 -0500

    Added

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import "github.com/prasannayanamadala/projects/operations"
 
 func main() {
+	
 	operations.AddOperation()
 	operations.SubOperation()
 	operations.MulOperation()
 	operations.DivideOperation()
+	
 }
