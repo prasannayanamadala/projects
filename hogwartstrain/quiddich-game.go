7e2f4ac59583555ef86913577505e431038c39f8
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-24 23:05:12 -0500

    using for

--- hogwartstrain/quiddich-game.go
+++ hogwartstrain/quiddich-game.go
@@ -5,51 +5,13 @@ import "fmt"
 func GameScore() {
 
 	Gryffindor := make([]int,0,40)
-	Gryffindor = append(Gryffindor,1)
-	Gryffindor = append(Gryffindor,2)
-	Gryffindor = append(Gryffindor,3)
-	Gryffindor = append(Gryffindor,4)
-	Gryffindor = append(Gryffindor,5)
-	Gryffindor = append(Gryffindor,6)
-	Gryffindor = append(Gryffindor,7)
-	Gryffindor = append(Gryffindor,8)
-	Gryffindor = append(Gryffindor,9)
-	Gryffindor = append(Gryffindor,10)
-	Gryffindor = append(Gryffindor,11)
-	Gryffindor = append(Gryffindor,12)
-	Gryffindor = append(Gryffindor,13)
-	Gryffindor = append(Gryffindor,14)
-	Gryffindor = append(Gryffindor,15)
-	Gryffindor = append(Gryffindor,16)
-	Gryffindor = append(Gryffindor,17)
-	Gryffindor = append(Gryffindor,18)
-	Gryffindor = append(Gryffindor,19)
-	Gryffindor = append(Gryffindor,20)
-	Gryffindor = append(Gryffindor,21)
-	Gryffindor = append(Gryffindor,22)
-	Gryffindor = append(Gryffindor,23)
-	Gryffindor = append(Gryffindor,24)
-	Gryffindor = append(Gryffindor,25)
-	Gryffindor = append(Gryffindor,26)
-	Gryffindor = append(Gryffindor,27)
-	Gryffindor = append(Gryffindor,28)
-	Gryffindor = append(Gryffindor,29)
-	Gryffindor = append(Gryffindor,30)
-	Gryffindor = append(Gryffindor,31)
-	Gryffindor = append(Gryffindor,32)
-	Gryffindor = append(Gryffindor,33)
-	Gryffindor = append(Gryffindor,34)
-	Gryffindor = append(Gryffindor,35)
-	Gryffindor = append(Gryffindor,36)
-	Gryffindor = append(Gryffindor,37)
-	Gryffindor = append(Gryffindor,38)
-	Gryffindor = append(Gryffindor,39)
-	Gryffindor = append(Gryffindor,40)
+	for i:=1 ; i<=40 ;i++ {
+		Gryffindor = append(Gryffindor , i)
+	}
 	sum := 0
 	for i := 0;i < len(Gryffindor);i++ {
 		sum = sum + Gryffindor[i]
 	}
-	
 	fmt.Println("List of points scored by each student in group Gryffindor:",Gryffindor)
 	fmt.Println("Total points scored by group Gryffindor:",sum)
 
@@ -69,46 +31,9 @@ func GameScore() {
 	fmt.Println("Average points scored by group Gryffindor:",avg1)
 	 
 	Slytherin := make([]int,0,40)
-	 Slytherin = append(Slytherin,1)
-	 Slytherin = append(Slytherin,2)
-	 Slytherin = append(Slytherin,3)
-	 Slytherin = append(Slytherin,4)
-	 Slytherin = append(Slytherin,5)
-	 Slytherin = append(Slytherin,6)
-	 Slytherin = append(Slytherin,7)
-	 Slytherin = append(Slytherin,8)
-	 Slytherin = append(Slytherin,9)
-	 Slytherin = append(Slytherin,10)
-	 Slytherin = append(Slytherin,11)
-	 Slytherin = append(Slytherin,12)
-	 Slytherin = append(Slytherin,13)
-	 Slytherin = append(Slytherin,14)
-	 Slytherin = append(Slytherin,15)
-	 Slytherin = append(Slytherin,16)
-	 Slytherin = append(Slytherin,17)
-	 Slytherin = append(Slytherin,18)
-	 Slytherin = append(Slytherin,19)
-	 Slytherin = append(Slytherin,20)
-	 Slytherin = append(Slytherin,21)
-	 Slytherin = append(Slytherin,22)
-	 Slytherin = append(Slytherin,23)
-	 Slytherin = append(Slytherin,24)
-	 Slytherin = append(Slytherin,25)
-	 Slytherin = append(Slytherin,26)
-	 Slytherin = append(Slytherin,27)
-	 Slytherin = append(Slytherin,28)
-	 Slytherin = append(Slytherin,29)
-	 Slytherin = append(Slytherin,30)
-	 Slytherin = append(Slytherin,31)
-	 Slytherin = append(Slytherin,32)
-	 Slytherin = append(Slytherin,33)
-	 Slytherin = append(Slytherin,34)
-	 Slytherin = append(Slytherin,35)
-	 Slytherin = append(Slytherin,36)
-	 Slytherin = append(Slytherin,37)
-	 Slytherin = append(Slytherin,38)
-	 Slytherin = append(Slytherin,39)
-	 Slytherin = append(Slytherin,40)
+	 for i:=1; i<=40 ; i=i+1 {
+		 Slytherin = append(Slytherin, i)
+	 }
 	 
 	 sum1 := 0
 	 for i := 0;i < len(Slytherin);i++ {
@@ -131,7 +56,8 @@ func GameScore() {
 
 	avg2 := sum6/len(Slytherin)
 	fmt.Println("Average points scored by group Slytherin:",avg2)
- 
+	
+	//different way of appending values
 	Ravenclaw := make([]int,0,40)
 	Ravenclaw = append(Ravenclaw,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40)
 	
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "github.com/prasannayanamadala/projects/howzatt"
+import 
 
-//"github.com/prasannayanamadala/projects/hogwartstrain"
+//"github.com/prasannayanamadala/projects/howzatt"
+
+"github.com/prasannayanamadala/projects/hogwartstrain"
  
 //"github.com/prasannayanamadala/projects/calculator"
 
@@ -12,7 +14,7 @@ func main() {
 	//operations.SubOperation()
 	//operations.MulOperation()
 	//operations.DivideOperation()
-	//game.GameScore()
-	worldcup.ScoreCard()
+	game.GameScore()
+	//worldcup.ScoreCard()
 
 }
