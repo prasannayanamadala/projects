2a2f102d700ac16a563e67df2a228f0829f4dfab
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-28 11:38:22 -0500

    using Structures

--- hogwartstrain/quiddich-game.go
+++ hogwartstrain/quiddich-game.go
@@ -101,32 +101,31 @@ func GameScore() {
 
 		G3.avg = float32(sum7/len(G3.pointsscored))
 		fmt.Println("Average points scored by group Ravenclaw:",G3.avg)
-	
-	//without using structures
-	
-	Hufflepuff := make([]int,0,40)
-	Hufflepuff = append(Hufflepuff,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,29,35,36,37,38,39,40)
+
+		var G4 Hogwarts
+		G4.Name = "Hufflepuff"
+		G4.pointsscored = make([]int,0,40)
+		G4.pointsscored = append(G4.pointsscored,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,29,35,36,37,38,39,40)
 	
 	sum3 := 0
-	for i := 0;i < len(Hufflepuff);i++ {
-		sum3 = sum3 + Hufflepuff[i]
+	for i := 0;i < len(G4.pointsscored);i++ {
+		sum3 = sum3 + G4.pointsscored[i]
 	}
-	fmt.Println("List of points scored by each student in group Hufflepuff:",Hufflepuff)
+	fmt.Println("List of points scored by each student in group Hufflepuff:",G4.pointsscored)
 	fmt.Println("Total points scored by group Hufflepuff:",sum3)
 
-	for i := 0; i < len(Hufflepuff);i++ {
-		Hufflepuff[i] = Hufflepuff[i]+2;
+	for i := 0; i < len(G4.pointsscored);i++ {
+		G4.pointsscored[i] = G4.pointsscored[i]+2;
 	}
-	fmt.Println("List of points scored by each student in group Hufflepuff:",Hufflepuff)
+	fmt.Println("List of points scored by each student in group Hufflepuff:",G4.pointsscored)
 	
 	sum8 := 0
-	for i := 0;i < len(Hufflepuff);i++ {
-		sum8 = sum8 + Hufflepuff[i]
+	for i := 0;i < len(G4.pointsscored);i++ {
+		sum8 = sum8 + G4.pointsscored[i]
 	}
 	fmt.Println("Total points scored by group Hufflepuff:",sum8)
-	
-	avg4 := sum8/len(Hufflepuff)
-	fmt.Println("Average points scored by group Hufflepuff:",avg4)
-	fmt.Println(G1.Name,G2.Name,G3.Name)
+	G4.avg = float32(sum8/len(G4.pointsscored))
+	fmt.Println("Average points scored by group Hufflepuff:",G4.avg)
+	fmt.Println("Names of all four Groups in quiddich game:",G1.Name,G2.Name,G3.Name,G4.Name)
 }
  
